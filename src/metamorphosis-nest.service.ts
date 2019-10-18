d91a5077e5c4c93628fffeac6e027f045ed70e64
fabio.formosa <fabio.formosa@gmail.com>
2019-10-19 01:19:30 +0200

    added support for typegoose
    Now it's possible to convert from a mongoose model to a dto

--- src/metamorphosis-nest.service.ts
+++ src/metamorphosis-nest.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { ConversionService } from '@fabio.formosa/metamorphosis';
+import { getClassForDocument } from '@typegoose/typegoose';
+import { Model } from 'mongoose';
 
 
 @Injectable()
@@ -8,7 +10,12 @@ export class MetamorphosisNestService {
   private conversionService: ConversionService = new ConversionService();
 
   public convert(sourceObj: any, targetClass:{ new(...args: any): any }):any{
-    return this.conversionService.convert(sourceObj, targetClass);
+    if(sourceObj instanceof Model){
+      const actualSourceType = getClassForDocument(sourceObj) || sourceObj.constructor;
+      return this.conversionService.convertBySource(sourceObj, actualSourceType, targetClass);
+    }
+    else
+      return this.conversionService.convert(sourceObj, targetClass);
   }
 
 }
