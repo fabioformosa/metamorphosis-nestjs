b22e952118c5e166403f45fd9bf087658fcb801c
fabio.formosa <fabio.formosa@gmail.com>
2019-10-29 00:27:14 +0100

    converted to dynamic module

--- src/metamorphosis.module.ts
+++ src/metamorphosis.module.ts
@@ -1,8 +1,24 @@
-import { Module } from '@nestjs/common';
+import { Module, DynamicModule } from '@nestjs/common';
 import { ConversionService } from './metamorphosis.service';
 
-@Module({
-  providers: [ConversionService],
-  exports: [ConversionService],
-})
-export class MetamorphosisModule {}
+// @Module({
+//   providers: [ConversionService],
+//   exports: [ConversionService],
+// })
+// export class MetamorphosisModule {}
+
+const ConversionServiceFactory = (logger: boolean = false) => ({
+  provide: 'ConversionService',
+  useFactory: () =>  new ConversionService(logger)
+});
+
+@Module({})
+export class MetamorphosisModule {
+  static register({logger}: {logger: boolean} = {logger: false}): DynamicModule {
+    return {
+      module: MetamorphosisModule,
+      providers: [ConversionServiceFactory(logger)],
+      exports: [ConversionService],
+    };
+  }
+}
