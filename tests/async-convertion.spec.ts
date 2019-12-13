7e4434f1e27d0b3950b6d180a7a6955e9c492e32
Fabio Formosa <fabio.formosa@dedagroup.it>
2019-12-13 12:24:49 +0100

    added test for convert all with promise

--- tests/async-convertion.spec.ts
+++ tests/async-convertion.spec.ts
@@ -28,4 +28,20 @@ describe('Async Conversion Suite', () => {
     expect(planet.name).toBe('saturn');
   });
 
+  it('test async convert all', async () => {
+    const planetDto1 = new PlanetDto();
+    planetDto1.id = '1';
+    planetDto1.name = 'saturn';
+    const planetDto2 = new PlanetDto();
+    planetDto2.id = '2';
+    planetDto2.name = 'jupiter';
+
+    const planets =  await Promise.all(conversionService.convertAll([planetDto1, planetDto2], Planet));
+    expect(planets.length).toBe(2);
+    expect(planets[0].id).toBe('1');
+    expect(planets[0].name).toBe('saturn');
+    expect(planets[1].id).toBe('2');
+    expect(planets[1].name).toBe('jupiter');
+  });
+
 });
