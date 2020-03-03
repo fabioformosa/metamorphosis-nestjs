161fd6f0bb0259c574c6700dcf378f74742d0b2b
fabio.formosa <fabio.formosa@gmail.com>
2020-03-03 22:54:07 +0100

    #15 imported typegoose metamorphosis plugin

--- src/metamorphosis.module.ts
+++ src/metamorphosis.module.ts
@@ -1,17 +1,18 @@
 import { Module, DynamicModule } from '@nestjs/common';
 import { ConversionService } from './metamorphosis.service';
+import { MetamorphosisPlugin } from '@fabio.formosa/metamorphosis';
 
-const ConversionServiceFactory = (logger : boolean | ((msg: string) => void) = false) => ({
+const ConversionServiceFactory = (logger : boolean | ((msg: string) => void) = false, plugins?: MetamorphosisPlugin[]) => ({
   provide: 'ConversionService',
-  useFactory: () =>  new ConversionService(logger)
+  useFactory: () =>  new ConversionService(logger, plugins)
 });
 
 @Module({})
 export class MetamorphosisModule {
-  static register({logger}: {logger: boolean | ((msg: string) => void)} = {logger: false}): DynamicModule {
+  static register({logger, plugins}: {logger: boolean | ((msg: string) => void), plugins?: MetamorphosisPlugin[]} = {logger: false, plugins: []}): DynamicModule {
     return {
       module: MetamorphosisModule,
-      providers: [ConversionServiceFactory(logger)],
+      providers: [ConversionServiceFactory(logger, plugins)],
       exports: [ConversionService],
     };
   }
--- src/metamorphosis.service.ts
+++ src/metamorphosis.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { ConversionHelper as MetamorphosisConversionService, logger} from '@fabio.formosa/metamorphosis';
+import { ConversionHelper as MetamorphosisConversionService, logger, MetamorphosisPlugin} from '@fabio.formosa/metamorphosis';
 import { getClass } from '@typegoose/typegoose/lib/internal/utils';
 import * as mongoose from 'mongoose';
 
@@ -9,23 +9,23 @@ export class ConversionService {
 
   private metamorphosisConversionService: MetamorphosisConversionService;
 
-  constructor(logger: boolean | ((msg: string) => void) = false){
-    this.metamorphosisConversionService = new MetamorphosisConversionService({logger});
+  constructor(logger: boolean | ((msg: string) => void) = false, plugins?: MetamorphosisPlugin[]){
+    this.metamorphosisConversionService = new MetamorphosisConversionService({logger, plugins});
   }
 
   public async convert(sourceObj: any, targetClass:{ new(...args: any): any }): Promise<unknown> {
-    if(sourceObj instanceof mongoose.Model || sourceObj instanceof mongoose.Schema.Types.Embedded 
-      || (sourceObj.constructor && sourceObj.constructor.name == 'EmbeddedDocument')
-      || (sourceObj.constructor && sourceObj.constructor.name == 'SingleNested')){
-      logger.log(`CONVERSION SERVICE - Typegoose support - detected sourceObj ${sourceObj.constructor.name} converting to ${targetClass.name}`);
-      const actualSourceType = getClass(sourceObj) || sourceObj.constructor;
-      logger.log(`CONVERSION SERVICE - Converting from actualSourceType ${actualSourceType.name} to ${targetClass.name}`);
-      return await this.metamorphosisConversionService.convertBySource(sourceObj, actualSourceType, targetClass);
-    }
-    else{
+    // if(sourceObj instanceof mongoose.Model || sourceObj instanceof mongoose.Schema.Types.Embedded 
+    //   || (sourceObj.constructor && sourceObj.constructor.name == 'EmbeddedDocument')
+    //   || (sourceObj.constructor && sourceObj.constructor.name == 'SingleNested')){
+    //   logger.log(`CONVERSION SERVICE - Typegoose support - detected sourceObj ${sourceObj.constructor.name} converting to ${targetClass.name}`);
+    //   const actualSourceType = getClass(sourceObj) || sourceObj.constructor;
+    //   logger.log(`CONVERSION SERVICE - Converting from actualSourceType ${actualSourceType.name} to ${targetClass.name}`);
+    //   return await this.metamorphosisConversionService.convertBySource(sourceObj, actualSourceType, targetClass);
+    // }
+    // else{
       logger.log(`CONVERSION SERVICE - Converting from ${sourceObj.constructor.name} to ${targetClass.name}`);
       return await this.metamorphosisConversionService.convert(sourceObj, targetClass);
-    }
+    // }
   }
 
   public convertAll(sourceArray: any[], itemTargetClass:{ new(...args: any): any }): Promise<unknown[]> {
